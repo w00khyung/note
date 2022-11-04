3db693f3911ba03291d6b4a90e6ce437e1aeaa59
ctd <qpflapffhs76@gmail.com>
2022-11-04 13:37:53 +0900

    property to name

--- seo-test/src/App.js
+++ seo-test/src/App.js
@@ -3,35 +3,31 @@ import './App.css';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 function App() {
-  const helmetContext = {};
   return (
-    <HelmetProvider context={helmetContext}>
+    <HelmetProvider>
       <Helmet>
         <title>SEO Test</title>
-        <meta property='og:type' content='website' />
-        <meta property='og:title' content='SEO Test' />
-        <meta property='og:description' content='SEO Test - Description' />
+        <meta name='og:title' content='SEO Test' />
+        <meta name='og:description' content='SEO Test - Description' />
         <meta
-          property='og:image'
+          name='og:image'
           content='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3f6fyQ3Sn3F6XGNFwethGoCoMD2RGBKhyffgL2H4FCw&s'
         />
       </Helmet>
-      <div>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Learn React
+        </a>
+      </header>
     </HelmetProvider>
   );
 }
