f21afbb20a89458bcc02885eee3d4d0f5ec450c5
ctd <qpflapffhs76@gmail.com>
2022-11-07 16:09:38 +0900

    React + S3 배포 테스트

---
+++ react-s3/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+      <Link to='/'>Go to Home</Link>
+    </div>
+  );
+};
+
+export default Login;
