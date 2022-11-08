93b9505084ed3689403f31096239ebe037cb7a84
ctd <qpflapffhs76@gmail.com>
2022-11-08 15:48:17 +0900

    🌱 add react-router-dom

--- react-boilerplate/src/App.tsx
+++ react-boilerplate/src/App.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from '../pages/Home';
 
 const App = () => {
-  return <h1>Hello, React!</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Router>
+  );
 };
 export default App;
