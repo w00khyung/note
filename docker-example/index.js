9efffa1640f421040a0fe117fc06e271f2e151b2
ctd <qpflapffhs76@gmail.com>
2022-11-18 17:43:09 +0900

    Docker Example

---
+++ docker-example/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, Docker! 🐬');
+});
+
+app.listen(8080, () => console.log('Server started!'));
